be947aa07e2114583dc2951d39ac8d49f6e91340
amansatija <satija.aman@gmail.com>
2018-01-01 08:33:57 +0530

    Update app.js
    commenting out fixed logs

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 //const CRYPTOCOMPARE_API_URI = "https://www.cryptocompare.com";
+//crypto compare api has changed and hence changing its base url to the latest version
 const CRYPTOCOMPARE_API_URI = "https://min-api.cryptocompare.com";
-//https://min-api.cryptocompare.com/
+
 const COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
 const UPDATE_INTERVAL = 60 * 1000;
 
@@ -14,12 +15,12 @@ let app = new Vue({
   methods: {
     getCoinData: function() {
       let self = this;
-
+      //changing api end point to its latest end (cryptocompare api has changed) ..
       axios.get(CRYPTOCOMPARE_API_URI + "/data/all/coinlist")
         .then((res) => {
             this.coinData = res.data.Data;
-            console.log(JSON.stringify("getting coin data."));
-            console.log(JSON.stringify(res));
+            //console.log(JSON.stringify("getting coin data."));
+            //console.log(JSON.stringify(res));
             this.getCoins();
           })
         .catch((err) => {
